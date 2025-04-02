3215f47e22af19476ef3fcad392bc065c2c1bf47
Ander Segovia <andersteven.castro@gmail.com>
2025-04-01 22:46:16 -0600

    Push end

--- pushNotificatinosPractice/app/src/main/java/com/example/pushnotificatinospractice/MainActivity.kt
+++ pushNotificatinosPractice/app/src/main/java/com/example/pushnotificatinospractice/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.pushnotificatinospractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("FCM", "Token: $token")
+
         }
-    }
+        }
 }
